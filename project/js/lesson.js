6c9fce28635edb7b87af1003a63706bc39d13e05
nsnakosgit <130403997+nsnakosgit@users.noreply.github.com>
2024-01-16 21:14:26 +0600

    Update lesson.js

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -14,3 +14,61 @@ phoneButton.addEventListener('click', () =>{
         phoneResult.style.color = 'red'
     }
 })
+
+
+
+
+const tapContents = document.querySelectorAll('.tab_content_block')
+const tabItems = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent  =() =>{
+    tapContents.forEach( (tabContent ) => {
+        tabContent.style.display = 'none'
+    })
+    tabItems.forEach((tabItem) =>{
+        tabItem.classList.remove( 'tab_content_item_active')
+    })
+}
+
+
+const  showTabContent = (index = 0) =>{
+    tapContents[index].style.display = 'block'
+    tabItems[index].classList.add('tab_content_item_active')
+}
+
+let currentIndex = 0;
+
+hideTabContent()
+showTabContent(currentIndex)
+
+const intervalId = setInterval(() => {
+    currentIndex = (currentIndex + 1) % tabItems.length;
+    hideTabContent()
+    showTabContent(currentIndex)
+}, 3000);
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabItems.forEach((tabItem,tabIndex) =>{
+            if (event.target === tabItem){
+                currentIndex = tabIndex;
+                hideTabContent()
+                showTabContent(currentIndex)
+            }
+        })
+    }
+}
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        setTimeout(() => {
+            tabItems.forEach((tabItem,tabIndex) =>{
+                if (event.target === tabItem){
+                    currentIndex = tabIndex;
+                    hideTabContent()
+                    showTabContent(currentIndex)
+                }
+            })
+        }, 3000);
+    }
+}
